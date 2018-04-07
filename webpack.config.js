022d323106b2bc9b7c6b7654b0435c77aca29d35
Dor Tzur <dortzur@gmail.com>
2018-04-07 11:31:11 +0300

    dev working!

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const isDev = (process.env.NODE_ENV = 'development');
+const externals = isDev
+  ? {}
+  : {
+      react: {
+        commonjs: 'react',
+        commonjs2: 'react',
+      },
+      'prop-types': {
+        commonjs: 'prop-types',
+        commonjs2: 'prop-types',
+      },
+    };
+module.exports = {
+  entry: isDev?'./demo/index.js':'./src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+  externals,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?name=images/[name].[ext]',
+          'image-webpack-loader?bypassOnDebug',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './demo/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
