d706ef4b73a568dcdb5029a72cbdfc8d24b71aaf
Dor Tzur <dortzur@gmail.com>
2018-11-16 10:36:32 +0200

    change consts to enums

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,17 @@
-export const SUITS = { CLUBS: 'C', DIAMONDS: 'D', HEARTS: 'H', SPADES: 'S' };
-export const RANKS = {
-  ACE: 'A',
-  TWO: '2',
-  THREE: '3',
-  FOUR: '4',
-  FIVE: '5',
-  SIX: '6',
-  SEVEN: '7',
-  EIGHT: '8',
-  NINE: '9',
-  TEN: 'T',
-  JACK: 'J',
-  QUEEN: 'Q',
-  KING: 'K',
-};
+export enum SUITS { CLUBS = 'C', DIAMONDS = 'D', HEARTS = 'H', SPADES = 'S' }
+
+export enum RANKS {
+  ACE = 'A',
+  TWO = '2',
+  THREE = '3',
+  FOUR = '4',
+  FIVE = '5',
+  SIX = '6',
+  SEVEN = '7',
+  EIGHT = '8',
+  NINE = '9',
+  TEN = 'T',
+  JACK = 'J',
+  QUEEN = 'Q',
+  KING = 'K',
+}
