f43912a6b7e9d87d488f8be2d424922a36372798
Dor Tzur <dortzur@gmail.com>
2018-11-24 10:01:25 +0200

    update card props

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { FaceCard } from './face-card';
 import { NumberCard } from './number-card';
-// noinspection NpmUsedModulesInstalled
-import PropTypes from 'prop-types';
 import { CardRank, getCard } from '../utils/card-utils';
 import { RANKS, SUITS } from '../constants';
 
 const isFaceCard = (rank: CardRank) => ['king', 'queen', 'jack'].includes(rank.name);
 
-interface Props {
+export interface CardProps {
   rank: RANKS,
   suit: SUITS,
   className?: string,
   size: number
 }
 
-export function Card(props: Props) {
+export function Card(props: CardProps) {
   const { rank, suit, size, ...rest } = props;
   const card = getCard(rank, suit);
   if (isFaceCard(card.rank)) {
@@ -24,11 +22,6 @@ export function Card(props: Props) {
   return <NumberCard card={card} size={size} {...rest} />;
 }
 
-Card.propTypes = {
-  size: PropTypes.number,
-  rank: PropTypes.string,
-  suit: PropTypes.string,
-};
 
 Card.defaultProps = {
   size: 10,
